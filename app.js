46b921b3d3a0864aa5ef35279dad1f8138c8e57e
sabrow <sabrow@hotmail.fr>
2022-03-24 21:34:02 +0100

    ajout accordion et js

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
-//validation formulaire
-let form = document.querySelector('form');
-form.addEventListener('submit', function(event)
-{    alert("formulaire envoyé");
-})
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
