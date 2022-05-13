9ac3666a468e400fce4f3d6f8f223692233fea63
sabrow <sabrow@hotmail.fr>
2022-05-13 08:34:41 +0200

    début js pour formulaire

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
+let acc = document.getElementsByClassName("accordion");
+let i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
@@ -11,4 +11,22 @@ for (i = 0; i < acc.length; i++) {
       panel.style.display = "block";
     }
   });
-}
+};
+//on cible nos elements du dom
+let firstname = document.getElementById('firstname');
+let lastname = document.getElementById('lastname');
+let email = document.getElementById('email');
+
+//on fait apparaitre le détail de ces elements dans la console avec .dir
+console.dir('firstname');
+console.dir('lastname');
+console.dir('email');
+
+//on observe les saisies des value de text en temps reel des champs
+firstname.addEventListener('input', function(){
+  console.log(firstname.value)
+})
+
+//assigner une valeur à la propriété "value " dans le html element
+//on impose à l'utilisateur d'inserer dans le champs email la valeur 'gmail.com'
+email.value = 'gmail.com';
